9abf8daa5b3b6ee0ae62dd5cd77e362fb459e19e
Joshua Eichorn <joshua.eichorn@pagely.com>
2014-01-27 16:09:06 -0600

    php 5.3 compat + bug fixes and status output

--- src/Shipper/Shipper.php
+++ src/Shipper/Shipper.php
@@ -9,14 +9,14 @@ class Shipper
     protected $config;
     protected $proc;
     protected $regex;
-    protected $map = [];
-    protected $destination = 'main';
+    protected $map = array();
+    protected $destination = array();
     protected $domain;
-    protected $files = [];
+    protected $files = array();
     protected $date;
     protected $time;
     protected $rollTime;
-    protected $http = [];
+    protected $http = array();
 
     public function __construct($config)
     {
@@ -57,7 +57,7 @@ class Shipper
     protected function buildDomainRegex()
     {
         $i = 4;
-        $parts = [];
+        $parts = array();
         foreach($this->config['domains'] as $domain => $dest)
         {
             $this->map[$i++] = $dest;
@@ -74,15 +74,18 @@ class Shipper
             $this->time = strtotime($match[1]);
             $this->date = date('Y-m-d', $this->time);
             $this->rollTime = date($this->config['rotate'], $this->time);
-            $this->domain = $match[3];
             foreach($match as $i => $m)
             {
-                if ($i > 2 && $m === $match[2])
+                if ($i > 3 && !empty($m))
                 {
                     $this->destination = $this->map[$i];
                     break;
                 }
             }
+            if (isset($this->destination['name']))
+		    $this->domain = $this->destination['name'];
+	    else
+		    $this->domain = $match[3];
 
             return true;
         }
@@ -95,15 +98,23 @@ class Shipper
         // check if we have an open file
         if (!isset($this->files[$this->domain]))
         {
+	    echo "Found line for $this->domain\n";
             $this->files[$this->domain] = new \stdClass;
             $this->files[$this->domain]->handle = gzopen($this->filename($this->domain), 'w');
             $this->files[$this->domain]->size = 0;
             $this->files[$this->domain]->time = $this->rollTime;
         }
-        elseif($this->rollTime !== $this->files[$this->domain]->time || $this->files[$this->domain]->size >= $this->config['maxsize'])
-        {
+        elseif($this->rollTime !== $this->files[$this->domain]->time)
+	{
+	    echo "Time based log roll $this->domain\n";
             $this->rollLog();
-            return $this->sendToDest();
+            return $this->sendToDest($line);
+        }
+ 	elseif($this->files[$this->domain]->size >= $this->config['maxsize'])
+        {
+	    echo "Size based log roll $this->domain\n";
+            $this->rollLog();
+            return $this->sendToDest($line);
         }
 
         $wrote = fwrite($this->files[$this->domain]->handle, $line, strlen($line));
@@ -126,9 +137,18 @@ class Shipper
 
     protected function upload()
     {
-        $conf = $this->config['destinations'][$this->destination];
-        $path = str_replace(['{domain}','{date}','{time}'], [$this->domain, $this->date, $this->time], $conf['path']);
+        $conf = $this->config['destinations'][$this->destination['destination']];
+	$keys = array('{domain}','{date}','{time}'); 
+	$values = array($this->domain, $this->date, $this->time);
+	foreach($this->destination as $k => $v)
+	{
+		$keys[] = '{'.$k.'}';
+		$values[] = $v;
+	}
+	
+        $path = str_replace($keys, $values, $conf['path']);
 
+	echo "Uploading $path\n";
         $request = $this->http()->put($path);
         $request->setBody(fopen($this->filename($this->domain), 'r'));
         $request->addHeader('Authorization', $conf['auth']);
@@ -161,6 +181,10 @@ class Shipper
             echo "Upload failed backing up log: $backup_file\n";
             rename($this->filename($this->domain), $backup_file);
         }
+	else
+	{
+	    echo "Upload complete\n";
+	}
     }
 
     protected function installSigHandler()
@@ -171,6 +195,7 @@ class Shipper
 
     protected function cleanup()
     {
+	echo "\nRuning cleanup\n";
         foreach($this->files as $domain => $o)
         {
             $this->domain = $domain;
@@ -180,10 +205,10 @@ class Shipper
 
     protected function http()
     {
-        if (!isset($this->http[$this->destination]))
-            $this->http[$this->destination] = new Client("http://{$this->config['destinations'][$this->destination]['host']}/");
+        if (!isset($this->http[$this->destination['destination']]))
+            $this->http[$this->destination['destination']] = new Client("http://{$this->config['destinations'][$this->destination['destination']]['host']}/");
 
-        return $this->http[$this->destination];
+        return $this->http[$this->destination['destination']];
     }
 
     public function signalHandler($signal)
